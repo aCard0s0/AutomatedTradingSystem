f1f8cd16eac997aa41e5b6f90f0dead7285ab8b9
André Cardoso <andre.fmarques.cardoso@gmail.com>
2023-12-28 13:12:12 +0000

    reorganize pom files

--- security-api-module/src/main/resources/db/migration/V2.1__CreateUserSecuritySchema.sql
+++ security-api-module/src/main/resources/db/migration/V2.1__CreateUserSecuritySchema.sql
@@ -1,13 +1,14 @@
+
 CREATE TABLE security_role
 (
-    id          BIGINT AUTO_INCREMENT PRIMARY KEY,
+    id          SERIAL PRIMARY KEY,
     role_name   varchar(100) DEFAULT NULL,
     description varchar(100) DEFAULT NULL
 );
 
 CREATE TABLE security_user
 (
-    id         BIGINT AUTO_INCREMENT PRIMARY KEY,
+    id         SERIAL PRIMARY KEY,
     email      varchar(255) NOT NULL,
     password   varchar(255) NOT NULL,
     first_name varchar(255) NOT NULL,
@@ -16,8 +17,8 @@ CREATE TABLE security_user
 
 CREATE TABLE user_role
 (
-    user_id BIGINT NOT NULL,
-    role_id BIGINT NOT NULL,
+    user_id SERIAL NOT NULL,
+    role_id SERIAL NOT NULL,
     CONSTRAINT FK_SECURITY_USER_ID FOREIGN KEY (user_id) REFERENCES security_user (id),
     CONSTRAINT FK_SECURITY_ROLE_ID FOREIGN KEY (role_id) REFERENCES security_role (id)
 );
